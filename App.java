365233701e61915e4ceabadab46ff79f980a30f5
Blake Womack and Stormi Hoebelheinrich <blake@>
2017-03-13 16:01:29 -0700

    added user input options.

--- App.java
+++ App.java
@@ -11,6 +11,22 @@ public class App {
     Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 200000, 3500);
 
     Vehicle[] allVehicles = {hatchback, suv, sedan, truck, crossover};
+
+ System.out.println("Welcome to our car dealership. What would you like to do? Enter one of the following options: All Vehicles or Search Price");
+
+ String navigationChoice = myConsole.readLine();
+
+    if (navigationChoice.equals("All Vehicles")){
+      for ( Vehicle individualVehicle : allVehicles ) {
+          System.out.println( "----------------------" );
+          System.out.println( individualVehicle.mYear );
+          System.out.println( individualVehicle.mBrand );
+          System.out.println( individualVehicle.mModel );
+          System.out.println( individualVehicle.mMiles );
+          System.out.println( individualVehicle.mPrice );
+      }
+    } else if (navigationChoice.equals("Search Price")){
+
     System.out.println("What is your maximum budget for a vehicle?");
     String stringUserMaxBudget = myConsole.readLine();
     int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
@@ -27,6 +43,8 @@ public class App {
      System.out.println( individualVehicle.mPrice );
    }
    }
-
+    } else {
+      System.out.println("I'm sorry, we don't recognize your input");
+    }
   }
 }
